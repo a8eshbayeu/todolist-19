b4d1047165e35ae74e4a8ef4ca35ede60b9be713
Artur Eshbayeu <a.eshbayeu@gmail.com>
2026-01-09 19:35:23 +0100

    fix: github pages routing and base

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import { App } from "@/app/App"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router"
 import { store } from "./app/store"
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/todolist-19">
     <Provider store={store}>
       <App />
     </Provider>
