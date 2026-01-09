b8fc5fad1076c96d472009377356e59af00144e7
Artur Eshbayeu <a.eshbayeu@gmail.com>
2026-01-09 20:11:44 +0100

    fix: HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router"
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/todolist-19">
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
